dcda2ec9fbd23697c40e8cd69ff180a0baa25b46
clairepanashetsuro <clairepanashetsuro@gmail.com>
2026-08-14 16:23:25 +0300

    Clean up user model by removing commented relationships
    Remove commented-out relationship fields for clarity.

--- ivhuRedu/models/user.py
+++ ivhuRedu/models/user.py
@@ -30,15 +30,10 @@ class User(Base):
     is_locked = Column(Boolean, default=False, nullable=False)
     locked_until = Column(DateTime(timezone=True), nullable=True)
 
-<<<<<<< HEAD
+
     extension_worker = relationship("ExtensionWorker", back_populates="user", uselist=False)
     farmer = relationship("Farmer", back_populates="user", uselist=False)
      
 
-
-=======
->>>>>>> caadafe (Merge pull request #5 from akirachix/feature/NYER-229-Location-endpoints)
-    # farmer = relationship("Farmer", back_populates="user", uselist=False)
-    # extension_worker = relationship("ExtensionWorker", back_populates="user", uselist=False)
 
 
